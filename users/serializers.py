fd8762eb4c4a10b319606858e050c1b75b5a7b76
angelica <angelicamariazm@gmail.com>
2023-01-04 18:50:41 -0300

    feat:rotas create,login,list e delete de Users

--- users/serializers.py
+++ users/serializers.py
@@ -10,12 +10,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "name",
+            "username",
             "email",
             "password",
             "phone_number",
             "city",
             "district",
             "is_superuser",
+            "is_superuser",
         ]
 
         extra_kwargs = {"password": {"write_only": True}}
+    
+    def create(self, validated_data: dict) -> User:
+        if validated_data["is_superuser"]:
+           return User.objects.create_superuser(**validated_data)
+        else:
+           return User.objects.create_user(**validated_data)  
+    
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
