4251e48ab07cfd534dd030830298286d10e9581c
David <davidkauandkad@outlook.com.br>
2023-01-09 19:08:23 -0300

    adicionando get

--- houses/urls.py
+++ houses/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("houses/<int:house_id>/sell/",views.SellHouseView.as_view()),
+    path("houses/sell/",views.SellHouseView.as_view()),
+    path("houses/<int:house_id>/sell/",views.SellHouseView.as_view()),
 ]
--- houses/views.py
+++ houses/views.py
@@ -3,7 +3,7 @@ from .serializers import SellHouseSerializer
 from rest_framework import generics
 from .models import SellHouse,House
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 
 
 class SellHouseView(generics.ListCreateAPIView):
@@ -13,6 +13,11 @@ class SellHouseView(generics.ListCreateAPIView):
     serializer_class = SellHouseSerializer
     queryset = SellHouse.objects.all()
     def perform_create(self, serializer):
-       house_id = self.kwargs['house_id']
-       house_obj = get_object_or_404(House,pk = house_id)
-       serializer.save(house = house_obj,buyer = self.request.user)
+        house_id = self.kwargs['house_id']
+        house_obj = get_object_or_404(House,pk = house_id)
+        serializer.save(house = house_obj,buyer = self.request.user)
+
+
+    def get_queryset(self):
+        return SellHouse.objects.filter(buyer_id = self.request.user.id)
+
