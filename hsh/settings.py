6a5813d8e8ef798fda71a399eae54dd353791eb3
Rafael <rafaelskz92@gmail.com>
2023-01-10 13:55:32 -0300

    Adicionando allowed_hosts

--- hsh/settings.py
+++ hsh/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-185d.up.railway.app/", "0.0.0.0"]
 
 
 # Application definition
