260f00d3c800ebd2bba6e2bc0fd7bfb9786999a2
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-01-11 14:36:10 -0300

    feat: comments-views-filter

--- comments/urls.py
+++ comments/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("comments/", views.CommentsView.as_view()),
     path("comments/<int:pk>", views.CommentsDetailView.as_view()),
+    path("comments/house/<int:house_id>", views.CommentsHouseView.as_view()),
+    path("comments/user/<int:user_id>", views.CommentsUserView.as_view()),
 ]
--- comments/views.py
+++ comments/views.py
@@ -4,6 +4,7 @@ from .serializers import CommentsSerializer
 from .models import Comments
 from .permissions import isCommentOwner
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView, Request, Response
 
 
 class CommentsView(ListCreateAPIView):
@@ -16,9 +17,26 @@ class CommentsView(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user.id)
 
+
 class CommentsDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly, isCommentOwner]
 
     serializer_class = CommentsSerializer
     queryset = Comments.objects.all()
+
+
+class CommentsHouseView(APIView):
+    def get(self, request: Request, house_id: int) -> Response:
+        comments = Comments.objects.filter(house_id=house_id)
+        serializer = CommentsSerializer(comments, many=True)
+
+        return Response(serializer.data)
+
+
+class CommentsUserView(APIView):
+    def get(self, request: Request, user_id: int) -> Response:
+        comments = Comments.objects.filter(user_id=user_id)
+        serializer = CommentsSerializer(comments, many=True)
+
+        return Response(serializer.data)
